66b8929bd88994cd95aafa17672099b81b7148fd
bianca990 <74793865+bianca990@users.noreply.github.com>
2020-12-08 22:40:55 +0200

    Implementare filtrare cuvinte cheie
    Issue: #9
    Implementeaza filtrarea fisierelor dupa cuvinte cheie. Parcurge directorul, citeste fiecare fisier si 
    apoi verifica daca fisierul contine cuvintele cheie. Fisierele care contin cuvintele cheie sunt 
    copiate in folderul KeywordFilteredFiles, cu structura directoarelor pastrata

--- src/ro/mta/facc/webcrawler/filter/KeywordFilter.java
+++ src/ro/mta/facc/webcrawler/filter/KeywordFilter.java
@@ -2,12 +2,103 @@ package ro.mta.facc.webcrawler.filter;
 
 import ro.mta.facc.webcrawler.config.WebCrawlerConfig;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Aceasta clasa se ocupa de filtrarea fisierelor dintr-un anumit director in functie de o lista de cuvinte cheie
  */
 public class KeywordFilter {
 
-    public static void filter(String directoryPath, WebCrawlerConfig crawlerConfig) {
+     private void emptyDirectory(File dir){
 
+        for (File file : dir.listFiles()){
+            if (file.isDirectory())
+            {
+                emptyDirectory(file);
+            }
+            file.delete();
+        }
+    }
+
+    private void emptyDefaultDir(String root){
+
+        File dir = new File(Paths.get(root,"KeywordFilteredFiles").toString());
+
+        if (!dir.exists()){
+            return;
+        }
+
+        emptyDirectory(dir);
+
+        dir.delete();
+    }
+
+
+    private void moveFilteredFiles(List<Path> filteredFiles, String baseDir) throws IOException {
+
+        final Path filteredDirPath = Path.of(baseDir,"KeywordFilteredFiles");
+
+        if (!Files.exists(filteredDirPath)) {
+            Files.createDirectory(filteredDirPath);
+        }
+
+        filteredFiles.forEach(filePath->{
+
+            String absPath = filePath.toAbsolutePath().toString();
+
+            absPath = absPath.replace(baseDir,filteredDirPath.toString());
+
+                try {
+                Path p = Paths.get(absPath);
+
+
+                if (!Files.exists(p.getParent())){
+                    new File(p.toString()).mkdirs();
+                }
+
+                Files.copy(filePath,p, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+    
+    
+    public static void filter(String directoryPath, WebCrawlerConfig crawlerConfig) {
+        emptyDefaultDir(directoryPath);
+
+        final List<String> keywords = crawlerConfig.getKeywords();
+
+        Path startPath = Path.of(directoryPath);
+
+        List<Path> filteredFiles = Files.find(startPath,99,(path, attrs)->{
+
+            try {
+                if (Files.isDirectory(path)){
+                    return false;
+                }
+                List<String> lines = Files.readAllLines(path);
+
+                String text = String.join("\n", lines);
+
+                lines.clear();
+
+                return keywords.stream().anyMatch(text::contains);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+        }, FileVisitOption.FOLLOW_LINKS).collect(Collectors.toList());
+
+
+        moveFilteredFiles(filteredFiles,directoryPath);
     }
 }
