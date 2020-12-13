9b6107ac5895284e69d421adbe9a92d65c3ecbbc
MihaiBogatu <bogatumihai1@yahoo.com>
2020-12-13 11:44:17 +0200

    Filtrare Tip Fisier
    Issue: #29
    Prin intermediul listei de fisiere acceptate din comanda "crawl list"
    din Issue #22 si obiectul FileTypeFilter se realizeaza conditionarea
    descarcarii de fisiere in functie de extensiile de fisier permise.
    In urma extragerii filename-ului si a extensiei aceasta este comparata in
    cadrul functiei "filter" cu lista de fisiere admise. In cazul in care
    extensia nu se regaseste in lista admisa o exceptie tip IOException
    este aruncata cu un mesaj sugestiv.
    Signed-off-by: MihaiBogatu bogatumihai1@yahoo.com

--- src/ro/mta/facc/webcrawler/filter/FileTypeFilter.java
+++ src/ro/mta/facc/webcrawler/filter/FileTypeFilter.java
@@ -2,12 +2,38 @@ package ro.mta.facc.webcrawler.filter;
 
 import ro.mta.facc.webcrawler.config.WebCrawlerConfig;
 
+import java.util.List;
+
 /**
  * Aceasta clasa se ocupa de filtrarea fisierelor dintr-un anumit director in functie de tip
  */
 public class FileTypeFilter {
 
-    public static void filter(String directoryPath, WebCrawlerConfig crawlerConfig) {
+    public static boolean filter(String directoryPath, WebCrawlerConfig crawlerConfig) {
+        List<String> fileTypes = crawlerConfig.getAcceptedFileTypes();
+        boolean checkedType = false;
+        String extension;
 
+        if (directoryPath.lastIndexOf(".") != -1 && directoryPath.lastIndexOf(".")!=0)
+        {
+            extension =directoryPath.substring(directoryPath.lastIndexOf(".")+1);
+        }
+        else
+        {
+            extension = "";
+        }
+
+
+        for (int i=0; i<fileTypes.size(); i++)
+        {
+            if (extension == fileTypes.get(i))
+            {
+                checkedType = true;
+                break;
+            }
+        }
+
+
+        return checkedType;
     }
 }
